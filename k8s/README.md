7be005aba89ef714a6f78595e32d9620d342f04d
Imron Alston <imron@imralsoftware.com>
2019-11-19 21:17:00 +1100

    rebase from master

--- k8s/README.md
+++ k8s/README.md
@@ -15,11 +15,11 @@ should try both with and without compression to see which provides the best thro
 ## How to run:
 
 1. Install `git` (e.g. apt-get install git.  Note for GKE customers: GKE CloudShell has `git` pre-installed)
-2. `git clone https://github.com/scalyr/agent-poc.git`
+2. `git clone https://github.com/scalyr/agent-poc.git --branch sidecar`
 3. `cd agent-poc/k8s/`
 4. `export SCALYR_API_KEY=<YOUR_API_KEY>`
 5. Edit `configmap.yaml` `SCALYR_K8S_CLUSTER_NAME`: Change `<your-cluster-name>` to name of k8s cluster you wish to monitor.
-7. `source start.sh`
+7. `source start.sh` or `source sidecar.sh` to run in sidecar mode
 
 ## How to re-pull latest script version if you already downloaded before
 
@@ -29,5 +29,5 @@ should try both with and without compression to see which provides the best thro
 4. `git pull`
 5. Edit `configmap.yaml` `SCALYR_K8S_CLUSTER_NAME`: Change `<your-cluster-name>` to name of k8s cluster you wish to monitor.
 7. Copy other custom changes you made from `configmap.yaml.bak` to `configmap.yaml` as needed.
-8. `source start.sh
+8. `source start.sh` or `source sidecar.sh` to run in sidecar mode
 
---
+++ k8s/sidecar.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+########################################################################
+# This script starts the k8s daemonset
+# 
+# Instructions:
+# a) set your Scalyr write key in your shell ( e.g. export SCALYR_API_KEY=<YOUR_SCALYR_KEY> )
+# b) edit configmap.yaml to set cluster name (to match the k8s cluster you want to monitor)
+# c) edit configmap.yaml to set `SCALYR_K8S_RATELIMIT_CLUSTER_NUM_AGENTS` to the approximate number of Kubernetes Nodes in your cluster.
+# d) source ./start.sh <configmap_file_name>
+########################################################################
+
+
+CONFIGMAP_FILE="configmap.yaml"
+
+if [ -n "$1" ]; then
+    CONFIGMAP_FILE=$1
+fi
+
+if [ -z "$SCALYR_API_KEY" ]; then
+    echo "Please set SCALYR_API_KEY environment variable.  Exiting."
+    return
+else
+    # delete old objects
+    echo "Deleting old scalyr daemonset ..."
+    kubectl delete daemonset scalyr-agent-2
+
+    echo "Deleting old scalyr configmap ..."
+    kubectl delete configmap scalyr-config
+
+    echo "Deleting old scalyr service account ..."
+    kubectl delete -f https://raw.githubusercontent.com/scalyr/scalyr-agent-2/release/k8s/scalyr-service-account.yaml
+
+    echo "Deleting old scalyr api-key secret ..."
+    kubectl delete secret scalyr-api-key
+
+    # Create Scalyr API key secret from environment variable
+    echo "Creating scalyr api-key secret ..."
+    kubectl create secret generic scalyr-api-key --from-literal=scalyr-api-key=${SCALYR_API_KEY}
+
+    # Authorize Scalyr agent pods
+    echo "Creating scalyr service account ..."
+    kubectl create -f https://raw.githubusercontent.com/scalyr/scalyr-agent-2/release/k8s/scalyr-service-account.yaml
+
+    # Create the Scalyr config map
+    echo "Creating scalyr config map ..."
+    kubectl create -f ${CONFIGMAP_FILE}
+    kubectl get configmap scalyr-config -o yaml
+
+    # Create the daemonset
+    echo "Creating scalyr daemon set ..."
+    kubectl create -f ./genlog.yaml
+fi
